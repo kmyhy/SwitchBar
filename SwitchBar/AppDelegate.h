17748e61aa69b4c2814009954a5f4e9050cd70b2
kmyhy <MY_FANGXIAN@example.com>
2017-02-20 09:40:42 +0800

    Initial Commit

---
+++ SwitchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwitchBar
+//
+//  Created by qq on 2017/2/20.
+//  Copyright © 2017年 qq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwitchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwitchBar
+//
+//  Created by qq on 2017/2/20.
+//  Copyright © 2017年 qq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
